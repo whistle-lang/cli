5217760dfc4580eb87547dd6a07d8845b8544ea8
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-01-03 09:05:43 -0500

    feat: lsp

---
+++ src/lsp/lsp.rs
@@ -0,0 +1,129 @@
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer};
+
+#[derive(Debug)]
+pub struct WhistleBackend {
+    pub client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for WhistleBackend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
+                completion_provider: Some(CompletionOptions::default()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "Whistle language server initialized")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    // async fn did_change_workspace_folders(&self, _: DidChangeWorkspaceFoldersParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "workspace folders changed!")
+    //         .await;
+    // }
+
+    // async fn did_change_configuration(&self, _: DidChangeConfigurationParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "configuration changed!")
+    //         .await;
+    // }
+
+    // async fn did_change_watched_files(&self, _: DidChangeWatchedFilesParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "watched files have changed!")
+    //         .await;
+    // }
+    // async fn did_open(&self, _: DidOpenTextDocumentParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "file opened!")
+    //         .await;
+    // }
+
+    // async fn did_change(&self, _: DidChangeTextDocumentParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "file changed!")
+    //         .await;
+    // }
+
+    // async fn did_save(&self, _: DidSaveTextDocumentParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "file saved!")
+    //         .await;
+    // }
+
+    // async fn did_close(&self, _: DidCloseTextDocumentParams) {
+    //     self.client
+    //         .log_message(MessageType::INFO, "file closed!")
+    //         .await;
+    // }
+    async fn completion(&self, _: CompletionParams) -> Result<Option<CompletionResponse>> {
+        Ok(Some(CompletionResponse::Array(vec![
+            //types
+            CompletionItem::new_simple("i32".to_string(), "32-bit signed integer type".to_string()),
+            CompletionItem::new_simple("i64".to_string(), "64-bit signed integer type".to_string()),
+            CompletionItem::new_simple(
+                "u32".to_string(),
+                "32-bit unsigned integer type".to_string(),
+            ),
+            CompletionItem::new_simple(
+                "u64".to_string(),
+                "64-bit unsigned integer type".to_string(),
+            ),
+            CompletionItem::new_simple("f32".to_string(), "32-bit floating point type".to_string()),
+            CompletionItem::new_simple("f64".to_string(), "64-bit floating point type".to_string()),
+            CompletionItem::new_simple("str".to_string(), "string".to_string()),
+            CompletionItem::new_simple("char".to_string(), "single character".to_string()),
+            CompletionItem::new_simple("bool".to_string(), "boolean".to_string()),
+            CompletionItem::new_simple("none".to_string(), "no value".to_string()),
+            //keywords
+            CompletionItem::new_simple("import".to_string(), "import declaration".to_string()),
+            CompletionItem::new_simple("extern".to_string(), "import external functions from a namespace".to_string()),
+            CompletionItem::new_simple("fn".to_string(), "declares a function".to_string()),
+            CompletionItem::new_simple("export".to_string(), "export declaration".to_string()),
+            CompletionItem::new_simple("return".to_string(), "return statement".to_string()),
+            CompletionItem::new_simple("if".to_string(), "if statement".to_string()),
+            CompletionItem::new_simple("else".to_string(), "else statement".to_string()),
+            CompletionItem::new_simple("while".to_string(), "while statement".to_string()),
+            CompletionItem::new_simple("break".to_string(), "break statement".to_string()),
+            CompletionItem::new_simple("continue".to_string(), "continue statement".to_string()),
+            CompletionItem::new_simple(
+                "var".to_string(),
+                "declares a function-scoped or globally-scoped variable".to_string(),
+            ),
+            CompletionItem::new_simple(
+                "val".to_string(),
+                "declares a constant variable".to_string(),
+            ),
+        ])))
+    }
+
+    async fn hover(&self, _: HoverParams) -> Result<Option<Hover>> {
+        // let _markdown = MarkupContent {
+        //     kind: MarkupKind::Markdown,
+        //     value: [
+        //         "# Header",
+        //         "Some text",
+        //     ]
+        //     .join("\n"),
+        // };
+        Ok(Some(Hover {
+            contents: HoverContents::Scalar(MarkedString::String("".to_string())),
+            /// HoverContents::Markup(markdown),
+            range: None,
+        }))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use clap::{Parser, Subcommand};
 use std::fs;
 use std::time::Instant;
+use uuid::Uuid;
 use wasmer::{Instance, Module, Store};
 use wasmer_compiler_cranelift::Cranelift;
 use wasmer_wasi::WasiState;
-use uuid::Uuid;
 
+mod lsp;
 mod util;
+
+use lsp::WhistleBackend;
+
+use tower_lsp::{LspService, Server};
 
 #[derive(Debug, Parser)]
 #[command(name = "whiskey")]
@@ -29,11 +34,19 @@ enum Commands {
         path: String,
         /// output file
         #[arg(short = 'o', long = "output", value_name = "OUTPUT")]
-        output: String,
+        output: Option<String>,
     },
+
+    /// launches the lsp
+    Lsp,
+
+    /// Builds whistle project
+    #[command(arg_required_else_help = true)]
+    Build { path: String },
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Cli::parse();
 
     match args.command {
@@ -44,7 +57,8 @@ fn main() {
             let mut store = Store::new(Cranelift::default());
             let module = Module::new(&store, bytes).unwrap();
             let wasi_env = WasiState::new(&(Uuid::new_v4().to_string()))
-                .finalize(&mut store).unwrap();
+                .finalize(&mut store)
+                .unwrap();
             let import_object = wasi_env.import_object(&mut store, &module).unwrap();
             let instance = Instance::new(&mut store, &module, &import_object).unwrap();
             let memory = instance.exports.get_memory("memory").unwrap();
@@ -52,8 +66,12 @@ fn main() {
             let start = instance.exports.get_function("_start").unwrap();
             start.call(&mut store, &[]).unwrap();
         }
+        Commands::Build{ path: _ } => {
+            unimplemented!()
+        }
         Commands::Compile { path, output } => {
             let now = Instant::now();
+            let output = output.unwrap_or(path.replace(".whi", ".wasm"));
             let text =
                 fs::read_to_string(path).expect("Something went wrong, we can't read this file.");
             let bytes = util::compile(&text);
@@ -62,13 +80,18 @@ fn main() {
                 fs::write(output, wasm_text.as_bytes())
                     .expect("Something went wrong, we can't write this file.");
             } else {
-                fs::write(output, bytes)
-                .expect("Something went wrong, we can't write this file.");
+                fs::write(output, bytes).expect("Something went wrong, we can't write this file.");
             }
             println!(
                 "Operation complete! Took us about {} seconds.",
                 now.elapsed().as_secs_f64()
             );
+        }
+        Commands::Lsp => {
+            let stdin = tokio::io::stdin();
+            let stdout = tokio::io::stdout();
+            let (service, socket) = LspService::new(|client| WhistleBackend { client });
+            Server::new(stdin, stdout, socket).serve(service).await;
         }
     }
 }
